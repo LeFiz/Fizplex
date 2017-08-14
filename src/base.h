2d9fed13916da28cd36c1ab009c109cc03d3543c
LeFiz <yesmehfriend@gmail.com>
2017-08-14 17:15:13 +0200

    Add ETM-vector-update function which also handles fill-in

--- src/base.h
+++ src/base.h
@@ -11,7 +11,6 @@ class Base {
     Base();
     ColMatrix<m> base;
     void invert();
-    void updateUnfinishedEtas(int i);
   private:
     struct ETM {
       Vec eta;
@@ -19,6 +18,8 @@ class Base {
     };
     std::vector<ETM> etms;
     std::array<double,m> work;
+    void updateUnfinishedEtas(int i);
+    void updateVecWithETM(int i, Vec& vec);
 };
 
 
@@ -30,8 +31,8 @@ Base<m>::Base() {
 
 template <int m>
 void Base<m>::invert() {
-  etms.clear();
-  etms.reserve(m);
+//  etms.clear();
+//  etms.reserve(m);
   for(int i = 0; i < m; i++)
     etms.push_back({base.column(i), i});
   double mult;
@@ -40,57 +41,66 @@ void Base<m>::invert() {
       if(n.index == i) {
         mult =  - 1.0 / n.value;
         n.value = -1;
-        std::cout << "Mult = " << mult << std::endl;
+//        std::cout << "Mult = " << mult << std::endl;
         break;
       }
     }
     for(auto& n : etms[i].eta) {
       n.value *= mult;
     }
-    std::cout << "Eta:" << std::endl;
-    for(auto j = 0; j < m; j++)
-      std::cout << etms[j].eta;
+
+    etms[i].col = i;
 
     // Update v_i
     updateUnfinishedEtas(i);
 
-    std::cout << "Eta:" << std::endl;
-    for(auto j = 0; j < m; j++)
-      std::cout << etms[j].eta;
   }
+//  std::cout << "Eta:" << std::endl;
+//  for(auto j = 0; j < etms.size(); j++)
+//    std::cout << "i = " << j << " eta = " << etms[j].eta;
 };
 
 template <int m>
-void Base<m>::updateUnfinishedEtas(int finishedETM) {
-  double mult = 0.0;
-  bool found;
-  int posi = -1;
-  // Copy finishedETM eta vector to work array
-  for(const auto& entry : etms[finishedETM].eta)
+void Base<m>::updateVecWithETM(int i, Vec& vec) {
+  // Copy eta to work array TODO only do if found
+  for(const auto& entry : etms[i].eta)
     work[entry.index] = entry.value;
 
-  // Update v_i in that are saved in etas
-  for(int i = finishedETM + 1; i < etms.size(); i++) {
-    found = false;
-  // Find nonzero multiplier in v_i
-    for(auto& v : etms[i].eta) {
-      if(v.index == finishedETM) {
-        found = true;
-        mult = v.value;
-        posi = v.index;
-      }
-    }
-    if(found) { // if !found: v^i_finishedETM is zero, no update required
-      for(auto& v : etms[i].eta)
-        v.value += mult * work[v.index];
-
-      // v_i = v_i + eta_i * v_i except for pivot element, there it's v_i = eta_i * v_i, i = posi
-      etms[i].eta[posi].value -= mult;
+  double mult = 0.0;
+  bool found = false;
+  // Find nonzero multiplier in vec_i
+  for(auto& v : vec) {
+    if(v.index == etms[i].col) {
+      found = true;
+      mult = v.value;
+      v.value = 0; // otherwise v_col = v_col + v_col * mult; should be v_col = v_col * mult
     }
   }
-  // Reset work to 0
-  for(const auto& entry : etms[finishedETM].eta)
-    work[entry.index] = 0.0;
+  if(found) { // if !found: v^i_finishedETM is zero, no update required
+    for(auto& e : vec) {
+      if(work[e.index] != 0.0) {
+        e.value += work[e.index] * mult;
+        work[e.index] = 0.0;
+      }
+    }
+    for(auto& e : etms[i].eta) {
+      if(work[e.index] != 0.0) { // not handled already => we have fill in
+        vec.push_back({e.index, work[e.index] * mult});
+        work[e.index] = 0.0;
+//        std::cout << "Fill in!" << std::endl;
+      }
+    }
+
+    // v_i = v_i + eta_i * v_i except for pivot element, there it's v_i = eta_i * v_i, i = posi
+    work[etms[i].col] -= mult;
+  }
+}
+template <int m>
+void Base<m>::updateUnfinishedEtas(int finishedETM) {
+  // Update v_i in that are saved in etas
+  for(int i = finishedETM + 1; i < etms.size(); i++)
+    updateVecWithETM(finishedETM, etms[i].eta);
+
 };
 
 std::ostream& operator<<(std::ostream& os, Vec const& vec) {
