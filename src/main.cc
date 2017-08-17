49f8e39c4fb45316d936d3145389fc1e72fc4127
LeFiz <yesmehfriend@gmail.com>
2017-08-17 12:48:13 +0200

    Add SVector tests

--- src/main.cc
+++ src/main.cc
@@ -1,8 +1,29 @@
 #include <random>
+#include <iostream>
 
 #include "svector.h"
 #include "colmatrix.h"
 #include "base.h"
+
+#define EXPECT_TRUE(statement) (statement ? std::cerr : (std::cerr << "Error! Expected true got false! In " << __FUNCTION__ << " (Line " << __LINE__ << " in " << __FILE__ << ")\n"))
+
+void test_svector() {
+  SVector v;
+  EXPECT_TRUE(v.length() == 0);
+  SVector vv = {{3,0.000f}, {19, 0}};
+  EXPECT_TRUE(v == vv);
+  v.add_value(3,-0.12345);
+  EXPECT_TRUE(v.length() == 1);
+  EXPECT_TRUE(v != vv);
+  EXPECT_TRUE(vv != v);
+
+  SVector w = {{0,-2.0}, {1,3}, {2,9}};
+  SVector ww = {{0,-2}, {1,3}, {2,9}, {7,0}};
+  EXPECT_TRUE(w.length() == 3);
+  EXPECT_TRUE(ww.length() == 4);
+  EXPECT_TRUE(w==ww);
+  EXPECT_TRUE(ww==w);
+}
 
 
 int main() {
@@ -29,6 +50,7 @@ int main() {
 //  b.base.add_value(0,1,0);
 //  b.base.add_value(1,0,3);
 //  b.base.add_value(1,1,2);
+  test_svector();
   Base<3> b;
   b.base.add_value(0,0,7);
   b.base.add_value(0,1,2);
@@ -41,5 +63,11 @@ int main() {
   std::cout << "Base:\n";
   std::cout << b.base;
   b.invert();
+  SVector v = {{0, 1}};
+  b.updateVec(v);
+  std::cout << v << std::endl;
+  SVector w = {{0,-2}, {1,3}, {2,9}, {7,0}};
+  std::cout << w << std::endl;
+  std::cout << (v==w) << std::endl;
   return 0;
 }
