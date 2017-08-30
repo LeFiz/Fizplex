f95f710609cdc772fa3128df6133b67a82729105
LeFiz <yesmehfriend@gmail.com>
2017-08-30 11:45:33 +0200

    Benchmark build time

--- build.py
+++ build.py
@@ -1,17 +1,20 @@
 from subprocess import call
+from time import time
 from termcolor import colored
 
 print colored("\nClang-Format ...\n", 'blue')
 call('clang-format-4.0 -i src/*.cc src/*.h', shell=True)
 
-print colored("\nCppCheck ...\n", 'blue')
+print colored("CppCheck ...\n", 'blue')
 call(['cppcheck', '--enable=all', '--inconclusive', '--suppress=missingIncludeSystem', '--inline-suppr', '--template=gcc', '--quiet', 'src'])
 
-print colored("\nMake clean ...\n", 'blue')
+print colored("Make clean ...\n", 'blue')
 call(['make', 'clean'])
 
-print colored("Make ...\n", 'blue')
+start=time();
+print colored("Make ...", 'blue')
 call(['make'])
+print "Make completed in ", time() - start, "s\n"
 
 print colored("Make run ...\n", 'blue')
 call(['make', 'run'])
