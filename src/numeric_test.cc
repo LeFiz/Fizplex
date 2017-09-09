0b6a2813f810d5723a4626b1bba2b2bdfbc8cd54
LeFiz <yesmehfriend@gmail.com>
2017-09-09 16:11:20 +0200

    Add is_ge() to numerics

--- src/numeric_test.cc
+++ src/numeric_test.cc
@@ -16,3 +16,19 @@ Test(Numeric, is_finite, "infinite values") {
   EXPECT(!is_finite(inf));
   EXPECT(!is_finite(-inf));
 }
+
+Test(Numeric, is_infinite, "infinite values") {
+  EXPECT(is_infinite(inf));
+  EXPECT(is_infinite(-inf));
+}
+
+Test(Numeric, is_infinite, "finite values") {
+  EXPECT(!is_infinite(2.00000f));
+  EXPECT(!is_infinite(31));
+  EXPECT(!is_infinite(-31.000000001f));
+}
+
+Test(Numeric, is_ge, "finite values") {
+  EXPECT(is_ge(1.0f, 0.0f));
+  EXPECT(is_ge(-2.111f, -999));
+}
