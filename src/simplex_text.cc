ab5d67192d6bc66a1ab1532b8b626c88ffc52f46
LeFiz <yesmehfriend@gmail.com>
2017-09-18 17:32:15 +0200

    Add tests for all col- and row-types

--- src/simplex_text.cc
+++ src/simplex_text.cc
@@ -1,40 +1,6 @@
 #include "simplex.h"
 #include "test.h"
 
-Test(Simplex, solve, "Ax <= b, x >= 0, optimal Solution exists") {
-  LP lp;
-  lp.add_column(ColType::LowerBound, 0, inf);
-  lp.add_column(ColType::LowerBound, 0, inf);
-  lp.add_column(ColType::LowerBound, 0, inf);
-  lp.add_row(RowType::LE, -inf, 14);
-  lp.add_row(RowType::LE, -inf, 28);
-  lp.add_row(RowType::LE, -inf, 30);
-
-  lp.add_value(0, 0, 2);
-  lp.add_value(0, 1, 1);
-  lp.add_value(0, 2, 1);
-  lp.add_value(1, 0, 4);
-  lp.add_value(1, 1, 2);
-  lp.add_value(1, 2, 3);
-  lp.add_value(2, 0, 2);
-  lp.add_value(2, 1, 5);
-  lp.add_value(2, 2, 5);
-
-  lp.add_logicals();
-
-  lp.add_obj_value(0, -1);
-  lp.add_obj_value(1, -2);
-  lp.add_obj_value(2, 3);
-
-  lp.set_b();
-
-  Simplex splx(lp);
-  splx.solve();
-
-  EXPECT(splx.get_result() == Simplex::Result::OptimalSolution);
-  EXPECT(is_eq(splx.get_z(), -13));
-}
-
 Test(Simplex, solve, "Ax <= b, x >= 0, unbounded") {
   LP lp;
   lp.add_column(ColType::LowerBound, 0, inf);
@@ -136,4 +102,102 @@ Test(Simplex, solve, "Ax >= b, x <= 0, bounded") {
 
   EXPECT(splx.get_result() == Simplex::Result::OptimalSolution);
   EXPECT(is_eq(splx.get_z(), -5.0f));
+}
+
+Test(Simplex, solve, "l <= Ax <= b, x >= 0, bounded") {
+  LP lp;
+  lp.add_column(ColType::Bounded, -8, 0);
+  lp.add_row(RowType::Range, -5, 5);
+
+  lp.add_value(0, 0, 2);
+
+  lp.add_logicals();
+
+  lp.add_obj_value(0, 5);
+
+  lp.set_b();
+
+  Simplex splx(lp);
+  splx.solve();
+
+  EXPECT(splx.get_result() == Simplex::Result::OptimalSolution);
+  EXPECT(is_eq(splx.get_z(), -12.5f));
+  EXPECT(splx.get_x() == DVector({-2.5f, 10}));
+}
+
+Test(Simplex, solve, "-inf <= Ax <= inf, x free, unbounded") {
+  LP lp;
+  lp.add_column(ColType::Free, -inf, inf);
+  lp.add_row(RowType::NonBinding, -inf, inf);
+
+  lp.add_value(0, 0, 1);
+
+  lp.add_logicals();
+
+  lp.add_obj_value(0, 1);
+
+  lp.set_b();
+
+  Simplex splx(lp);
+  splx.solve();
+
+  EXPECT(splx.get_result() == Simplex::Result::Unbounded);
+  EXPECT(is_infinite(splx.get_z()));
+  EXPECT(is_le(splx.get_z(), 0));
+  EXPECT(is_infinite(splx.get_x()[0]));
+}
+
+Test(Simplex, solve, "Ax <= b, x == 0, bounded") {
+  LP lp;
+  lp.add_column(ColType::Fixed, 0, 0);
+  lp.add_row(RowType::Range, -5, 5);
+
+  lp.add_value(0, 0, 1);
+
+  lp.add_logicals();
+
+  lp.add_obj_value(0, -1);
+
+  lp.set_b();
+
+  Simplex splx(lp);
+  splx.solve();
+
+  EXPECT(splx.get_result() == Simplex::Result::OptimalSolution);
+  EXPECT(is_eq(splx.get_z(), 0));
+  EXPECT(is_eq(splx.get_x()[0], 0));
+}
+
+Test(Simplex, solve, "Ax <= b, x >= 0, optimal Solution exists") {
+  LP lp;
+  lp.add_column(ColType::LowerBound, 0, inf);
+  lp.add_column(ColType::LowerBound, 0, inf);
+  lp.add_column(ColType::LowerBound, 0, inf);
+  lp.add_row(RowType::LE, -inf, 14);
+  lp.add_row(RowType::LE, -inf, 28);
+  lp.add_row(RowType::LE, -inf, 30);
+
+  lp.add_value(0, 0, 2);
+  lp.add_value(0, 1, 1);
+  lp.add_value(0, 2, 1);
+  lp.add_value(1, 0, 4);
+  lp.add_value(1, 1, 2);
+  lp.add_value(1, 2, 3);
+  lp.add_value(2, 0, 2);
+  lp.add_value(2, 1, 5);
+  lp.add_value(2, 2, 5);
+
+  lp.add_logicals();
+
+  lp.add_obj_value(0, -1);
+  lp.add_obj_value(1, -2);
+  lp.add_obj_value(2, 3);
+
+  lp.set_b();
+
+  Simplex splx(lp);
+  splx.solve();
+
+  EXPECT(splx.get_result() == Simplex::Result::OptimalSolution);
+  EXPECT(is_eq(splx.get_z(), -13));
 }
