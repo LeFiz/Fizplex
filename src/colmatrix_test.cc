d07762a57c98b49185c9c8ddaaa86a7892a0cc31
LeFiz <yesmehfriend@gmail.com>
2017-10-13 16:39:06 +0200

    Move colmatrix tests to gtest

--- src/colmatrix_test.cc
+++ src/colmatrix_test.cc
@@ -1,28 +1,28 @@
 
 #include "colmatrix.h"
-#include "test.h"
+#include "gtest/gtest.h"
 
-Test(ColMatrix, row_count, "empty matrix") {
+TEST(ColMatrix, RowCountEmptyMatrix) {
   ColMatrix m;
-  EXPECT(m.row_count() == 0);
+  EXPECT_TRUE(m.row_count() == 0);
 }
 
-Test(ColMatrix, row_count, "non-empty matrix") {
+TEST(ColMatrix, RowCountNonEmptyMatrix) {
   ColMatrix m(3, 4);
-  EXPECT(m.row_count() == 3);
+  EXPECT_TRUE(m.row_count() == 3);
 }
 
-Test(ColMatrix, col_count, "empty matrix") {
+TEST(ColMatrix, ColCountEmptyMatrix) {
   ColMatrix m;
-  EXPECT(m.col_count() == 0);
+  EXPECT_TRUE(m.col_count() == 0);
 }
 
-Test(ColMatrix, col_count, "non-empty matrix") {
+TEST(ColMatrix, ColCountNonEmptyMatrix) {
   ColMatrix m(3, 4);
-  EXPECT(m.col_count() == 4);
+  EXPECT_TRUE(m.col_count() == 4);
 }
 
-Test(ColMatrix, equality_operator, "equal matrices") {
+TEST(ColMatrix, EqualityOperatorForEqualMatrices) {
   ColMatrix m(3, 3,
               {{{0, 7}, {2, -3}},
                {{0, 2}, {2, 4}, {1, 3}},
@@ -31,13 +31,13 @@ Test(ColMatrix, equality_operator, "equal matrices") {
                {{{0, 7}, {2, -3}, {1, 0.0}},
                 {{0, 2}, {1, 3}, {2, 4}},
                 {{0, 1}, {1, -1}, {2, -2}}});
-  EXPECT(m == mm);
-  EXPECT(mm == m);
-  EXPECT(!(m != mm));
-  EXPECT(!(mm != m));
+  EXPECT_TRUE(m == mm);
+  EXPECT_TRUE(mm == m);
+  EXPECT_TRUE(!(m != mm));
+  EXPECT_TRUE(!(mm != m));
 }
 
-Test(ColMatrix, equality_operator, "inequal matrices, wrong dimensions") {
+TEST(ColMatrix, EqualityOperatorWrongDimensions) {
   ColMatrix m(4, 5,
               {{{0, 7}, {2, -3}},
                {{0, 2}, {2, 4}, {1, 3}},
@@ -46,35 +46,35 @@ Test(ColMatrix, equality_operator, "inequal matrices, wrong dimensions") {
                {{{0, 7}, {2, -3}, {1, 0.0}},
                 {{0, 2}, {1, 3}, {2, 4}},
                 {{0, 1}, {1, -1}, {2, -2}}});
-  EXPECT(!(m == mm));
-  EXPECT(!(mm == m));
-  EXPECT(m != mm);
-  EXPECT(mm != m);
+  EXPECT_TRUE(!(m == mm));
+  EXPECT_TRUE(!(mm == m));
+  EXPECT_TRUE(m != mm);
+  EXPECT_TRUE(mm != m);
 }
 
-Test(ColMatrix, assign_operator, "non-empty matrix") {
+TEST(ColMatrix, AssignmentOperator) {
   ColMatrix m(1, 1);
   ColMatrix mm(3, 3,
                {{{0, 7}, {2, -3}, {1, 0.0}},
                 {{0, 2}, {1, 3}, {2, 4}},
                 {{0, 1}, {1, -1}, {2, -2}}});
   m = mm;
-  EXPECT(m == mm);
+  EXPECT_TRUE(m == mm);
 }
 
-Test(ColMatrix, add_row, "empty matrix") {
+TEST(ColMatrix, AddRowToEmptyMatrix) {
   ColMatrix m;
   m.add_row();
-  EXPECT(m.row_count() == 1);
+  EXPECT_TRUE(m.row_count() == 1);
 }
 
-Test(ColMatrix, add_column, "empty matrix") {
+TEST(ColMatrix, AddColToEmptyMatrix) {
   ColMatrix m;
   m.add_column();
-  EXPECT(m.col_count() == 1);
+  EXPECT_TRUE(m.col_count() == 1);
 }
 
-Test(ColMatrix, add_column, "non-empty matrix; add values to new column") {
+TEST(ColMatrix, AddColumnAndValuesToNonEmptyMatrix) {
   ColMatrix m(3, 3,
               {{{0, 7}, {2, -3}, {1, 0.0}},
                {{0, 2}, {1, 3}, {2, 4}},
@@ -83,19 +83,19 @@ Test(ColMatrix, add_column, "non-empty matrix; add values to new column") {
   const double d = 2.111;
   m.add_value(1, 3, d);
   SVector v = {{1, d}};
-  EXPECT(v == m.column(3));
+  EXPECT_TRUE(v == m.column(3));
 }
 
-Test(ColMatrix, column, "non-empty matrix") {
+TEST(ColMatrix, GetColumn) {
   ColMatrix m(3, 3,
               {{{0, 7}, {2, -3}},
                {{0, 2}, {1, 3}, {2, 4}},
                {{0, 1}, {1, -1}, {2, -2}}});
   SVector v = {{0, 2}, {1, 3}, {2, 4}};
-  EXPECT(m.column(1) == v);
+  EXPECT_TRUE(m.column(1) == v);
 }
 
-Test(ColMatrix, swap_column, "non-empty matrix") {
+TEST(ColMatrix, SwapColumns) {
   ColMatrix m(3, 3,
               {{{0, 7}, {2, -3}},
                {{0, 2}, {1, 3}, {2, 4}},
@@ -103,6 +103,6 @@ Test(ColMatrix, swap_column, "non-empty matrix") {
   SVector v0 = m.column(0);
   SVector v2 = m.column(2);
   m.swap_columns(0, 2);
-  EXPECT(m.column(0) == v2);
-  EXPECT(m.column(2) == v0);
+  EXPECT_TRUE(m.column(0) == v2);
+  EXPECT_TRUE(m.column(2) == v0);
 }
