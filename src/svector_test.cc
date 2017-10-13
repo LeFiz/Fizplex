d3d8145ff82c2aa9c630328c276c873b48802e81
LeFiz <yesmehfriend@gmail.com>
2017-10-13 12:25:54 +0200

    Migrate svector to gtest

--- src/svector_test.cc
+++ src/svector_test.cc
@@ -1,77 +1,66 @@
 #include "svector.h"
-#include "test.h"
+//#include "test.h"
+#include "gtest/gtest.h"
 
-Test(SVector, length, "empty") {
+TEST(SVectorTest, EmptyHasZeolength) {
   SVector v;
-  EXPECT(v.length() == 0);
+  EXPECT_TRUE(v.length() == 0);
 }
 
-Test(SVector, length, "non-empty") {
-  SVector v = {{0, -2}, {1, 3}, {2, 9}, {7, -2.111111111111}};
-  EXPECT(v.length() == 4);
+TEST(SVectorTest, NonEmptyLength) {
+  SVector v = {{0, -2}, {1, 3}, {2, 9}, {7, -2.11}};
+  EXPECT_TRUE(v.length() == 4);
 }
 
-Test(SVector, add_value, "valid entry") {
-  SVector v = {{0, -2}, {1, 3}, {2, 9}, {7, -2.111111111111}};
+TEST(SVectorTest, AddValueInsertsNewValue) {
+  SVector v = {{0, -2}, {1, 3}, {2, 9}, {7, -2.11}};
   const double d = 3.14;
   v.add_value(3, d);
-  EXPECT(v.length() == 5);
-  EXPECT(is_eq(v.get_value(3), d));
+  EXPECT_TRUE(v.length() == 5);
+  EXPECT_TRUE(is_eq(v.get_value(3), d));
 }
 
-Test(SVector, add_value, "double entry") {
-  SVector v = {{0, -2}, {1, 3}, {2, 9}, {7, -2.111111111111}};
+TEST(SVectorTest, AddValueCanAddSameValueTwice) {
+  SVector v = {{0, -2}, {1, 3}, {2, 9}, {7, -2.11}};
   const double d = 3;
   v.add_value(1, d);
-  EXPECT(v.length() == 5);
+  EXPECT_TRUE(v.length() == 5);
 }
 
-Test(SVector, equality, "equal but different order") {
-  SVector v = {{0, -2}, {1, 3}, {2, 9}, {7, -2.111111111111}};
-  SVector w = {{2, 9}, {0, -2}, {7, -2.111111111111}, {1, 3}};
-  EXPECT(v == w);
-  EXPECT(w == v);
+TEST(SVectorTest, ReorderedVectorsAreEqual) {
+  SVector v = {{0, -2}, {1, 3}, {2, 9}, {7, -2.11}};
+  SVector w = {{2, 9}, {0, -2}, {7, -2.11}, {1, 3}};
+  EXPECT_TRUE(v == w);
+  EXPECT_TRUE(w == v);
+  EXPECT_TRUE(!(v != w));
+  EXPECT_TRUE(!(w != v));
 }
 
-Test(SVector, equality, "inequal, one empty") {
-  SVector v = {{0, -2}, {1, 3}, {2, 9}, {7, -2.111111111111}};
+TEST(SVectorTest, NonEmptyVecIsNotEqualToEmpty) {
+  SVector v = {{0, -2}, {1, 3}, {2, 9}, {7, -2.11}};
   SVector w;
-  EXPECT(!(v == w));
-  EXPECT(!(w == v));
+  EXPECT_TRUE(!(v == w));
+  EXPECT_TRUE(v != w);
 }
 
-Test(SVector, inequality, "equal but different order") {
-  SVector v = {{0, -2}, {1, 3}, {2, 9}, {7, -2.111111111111}};
-  SVector w = {{2, 9}, {0, -2}, {7, -2.111111111111}, {1, 3}};
-  EXPECT(!(v != w));
-  EXPECT(!(w != v));
-}
-
-Test(SVector, inequality, "inequal, one empty") {
-  SVector v = {{0, -2}, {1, 3}, {2, 9}, {7, -2.111111111111}};
-  SVector w;
-  EXPECT(v != w);
-  EXPECT(w != v);
-}
-
-Test(SVector, iterator, "nonempty, change values") {
-  SVector v = {{0, -2}, {1, 3}, {2, 9}, {7, -2.111111111111}};
+TEST(SVectorTest, ChangeValsThroughIteratorSetsRightVal) {
+  SVector v = {{0, -2}, {1, 3}, {2, 9}, {7, -2.11}};
   for (auto &n : v) {
     n.value *= 3;
   }
-  EXPECT(is_eq(v.get_value(2), 27));
+  EXPECT_TRUE(is_eq(v.get_value(2), 27));
 }
 
-Test(SVector, iterator, "empty") {
+TEST(SVectorTest, CanIterateEmptyVec) {
   SVector v;
   for (auto &n : v) {
     n.value *= 3;
-    EXPECT(false);
+    EXPECT_TRUE(false);
   }
 }
 
-Test(SVector, scalar_mult, "non-empty") {
+TEST(SVectorTest, ScalaMult) {
   SVector v = {{0, -2}, {1, 3}, {4, 9}};
   SVector w = {{0, -4}, {1, 6}, {4, 18}};
-  EXPECT(2.0f * v == w);
+  EXPECT_TRUE(2.0 * v == w);
 }
