4ae298dd42b6bc1d63dc87c960b3dcd80829aecb
LeFiz <yesmehfriend@gmail.com>
2017-10-26 14:51:17 +0200

    Add more verification tests; mark some as experimental (not run by default)

--- src/verification_test.cc
+++ src/verification_test.cc
@@ -19,3 +19,42 @@ TEST(VerificationTest, Sc105) {
   spx.solve();
   EXPECT_TRUE(is_eq(-5.2202061212E+01, spx.get_z()));
 }
+
+TEST(VerificationTest, Blend) {
+  MPSReader r("./test/blend.mps");
+  LP lp = r.read_lp();
+
+  Simplex spx(lp);
+  spx.solve();
+  EXPECT_TRUE(is_eq(-3.0812149846E+01, spx.get_z()));
+}
+
+TEST(ExperimentalVerificationTest, Brandy) {
+  MPSReader r("./test/brandy.mps");
+  LP lp = r.read_lp();
+
+  Simplex spx(lp);
+  spx.solve();
+  EXPECT_TRUE(is_eq(1.5185098965E+03, spx.get_z()));
+  EXPECT_DOUBLE_EQ(1.5185098965E+03, spx.get_z());
+}
+
+TEST(ExperimentalVerificationTest, Agg) {
+  MPSReader r("./test/agg.mps");
+  LP lp = r.read_lp();
+
+  Simplex spx(lp);
+  spx.solve();
+  EXPECT_TRUE(is_eq(-3.5991767287E+07, spx.get_z()));
+  EXPECT_DOUBLE_EQ(-3.5991767287E+07, spx.get_z());
+}
+
+TEST(ExperimentalVerificationTest, Stocfor1) {
+  MPSReader r("./test/stocfor1.mps");
+  LP lp = r.read_lp();
+
+  Simplex spx(lp);
+  spx.solve();
+  EXPECT_TRUE(is_eq(-4.1131976219E+04, spx.get_z()));
+  EXPECT_DOUBLE_EQ(-4.1131976219E+04, spx.get_z());
+}
