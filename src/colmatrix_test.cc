7f1f6e18b95d8569382704b14e9faebff6675e53
LeFiz <yesmehfriend@gmail.com>
2017-11-08 13:52:02 +0100

    Make colmatrix tests less verbose

--- src/colmatrix_test.cc
+++ src/colmatrix_test.cc
@@ -1,76 +1,64 @@
 #include "colmatrix.h"
 #include "gtest/gtest.h"
 
-TEST(ColMatrix, RowCountEmptyMatrix) {
-  ColMatrix m;
+TEST(ColMatrix, RowAndColCountEmptyMatrix) {
+  const ColMatrix m;
   EXPECT_TRUE(m.row_count() == 0);
-}
-
-TEST(ColMatrix, RowCountNonEmptyMatrix) {
-  ColMatrix m(3, 4);
-  EXPECT_TRUE(m.row_count() == 3);
-}
-
-TEST(ColMatrix, ColCountEmptyMatrix) {
-  ColMatrix m;
   EXPECT_TRUE(m.col_count() == 0);
 }
 
-TEST(ColMatrix, ColCountNonEmptyMatrix) {
-  ColMatrix m(3, 4);
+TEST(ColMatrix, RowAndColCountNonEmptyMatrix) {
+  const ColMatrix m(3, 4);
+  EXPECT_TRUE(m.row_count() == 3);
   EXPECT_TRUE(m.col_count() == 4);
 }
 
-TEST(ColMatrix, EqualityOperatorForEqualMatrices) {
-  ColMatrix m(3, 3,
-              {{{0, 7}, {2, -3}},
-               {{0, 2}, {2, 4}, {1, 3}},
-               {{0, 1}, {1, -1}, {2, -2}}});
-  ColMatrix mm(3, 3,
-               {{{0, 7}, {2, -3}, {1, 0.0}},
-                {{0, 2}, {1, 3}, {2, 4}},
-                {{0, 1}, {1, -1}, {2, -2}}});
-  EXPECT_TRUE(m == mm);
-  EXPECT_TRUE(mm == m);
-  EXPECT_TRUE(!(m != mm));
-  EXPECT_TRUE(!(mm != m));
+TEST(ColMatrix, EqualityOperatorsWhenZeroValIsAdded) {
+  const ColMatrix m(3, 3,
+                    {{{0, 7}, {2, -3}},
+                     {{0, 2}, {2, 4}, {1, 3}},
+                     {{0, 1}, {1, -1}, {2, -2}}});
+  ColMatrix m_with_zero_added = m;
+  m_with_zero_added.add_value(1, 0, 0.0);
+  EXPECT_TRUE(m == m_with_zero_added);
+  EXPECT_TRUE(m_with_zero_added == m);
+  EXPECT_FALSE(m != m_with_zero_added);
+  EXPECT_FALSE(m_with_zero_added != m);
 }
 
-TEST(ColMatrix, EqualityOperatorWrongDimensions) {
-  ColMatrix m(4, 5,
-              {{{0, 7}, {2, -3}},
-               {{0, 2}, {2, 4}, {1, 3}},
-               {{0, 1}, {1, -1}, {2, -2}}});
-  ColMatrix mm(3, 3,
-               {{{0, 7}, {2, -3}, {1, 0.0}},
-                {{0, 2}, {1, 3}, {2, 4}},
-                {{0, 1}, {1, -1}, {2, -2}}});
-  EXPECT_TRUE(!(m == mm));
-  EXPECT_TRUE(!(mm == m));
-  EXPECT_TRUE(m != mm);
-  EXPECT_TRUE(mm != m);
+TEST(ColMatrix, EqualityOperatorsSameMatrixButWrongDimensions) {
+  const ColMatrix m45(4, 5,
+                      {{{0, 7}, {2, -3}},
+                       {{0, 2}, {2, 4}, {1, 3}},
+                       {{0, 1}, {1, -1}, {2, -2}}});
+  const ColMatrix m33(3, 3,
+                      {{{0, 7}, {2, -3}},
+                       {{0, 2}, {1, 3}, {2, 4}},
+                       {{0, 1}, {1, -1}, {2, -2}}});
+  EXPECT_FALSE(m45 == m33);
+  EXPECT_TRUE(m45 != m33);
 }
 
 TEST(ColMatrix, AssignmentOperator) {
-  ColMatrix m(1, 1);
-  ColMatrix mm(3, 3,
-               {{{0, 7}, {2, -3}, {1, 0.0}},
-                {{0, 2}, {1, 3}, {2, 4}},
-                {{0, 1}, {1, -1}, {2, -2}}});
-  m = mm;
-  EXPECT_TRUE(m == mm);
+  ColMatrix m11(1, 1);
+  const ColMatrix m33(3, 3,
+                      {{{0, 7}, {2, -3}, {1, 0.0}},
+                       {{0, 2}, {1, 3}, {2, 4}},
+                       {{0, 1}, {1, -1}, {2, -2}}});
+  m11 = m33;
+  EXPECT_EQ(m33, m11);
 }
 
 TEST(ColMatrix, AddRowToEmptyMatrix) {
   ColMatrix m;
   m.add_row();
-  EXPECT_TRUE(m.row_count() == 1);
+  EXPECT_EQ(1u, m.row_count());
 }
 
 TEST(ColMatrix, AddColToEmptyMatrix) {
   ColMatrix m;
   m.add_column();
-  EXPECT_TRUE(m.col_count() == 1);
+  EXPECT_EQ(1u, m.col_count());
 }
 
 TEST(ColMatrix, AddColumnAndValuesToNonEmptyMatrix) {
@@ -78,20 +66,9 @@ TEST(ColMatrix, AddColumnAndValuesToNonEmptyMatrix) {
               {{{0, 7}, {2, -3}, {1, 0.0}},
                {{0, 2}, {1, 3}, {2, 4}},
                {{0, 1}, {1, -1}, {2, -2}}});
-  m.add_column();
-  const double d = 2.111;
-  m.add_value(1, 3, d);
-  SVector v = {{1, d}};
-  EXPECT_TRUE(v == m.column(3));
-}
-
-TEST(ColMatrix, GetColumn) {
-  ColMatrix m(3, 3,
-              {{{0, 7}, {2, -3}},
-               {{0, 2}, {1, 3}, {2, 4}},
-               {{0, 1}, {1, -1}, {2, -2}}});
-  SVector v = {{0, 2}, {1, 3}, {2, 4}};
-  EXPECT_TRUE(m.column(1) == v);
+  const SVector new_col = {{0, 1.0}, {2, 2.0}, {3, 3.0}};
+  m.add_column(new_col);
+  EXPECT_EQ(new_col, m.column(3));
 }
 
 TEST(ColMatrix, SwapColumns) {
@@ -99,9 +76,9 @@ TEST(ColMatrix, SwapColumns) {
               {{{0, 7}, {2, -3}},
                {{0, 2}, {1, 3}, {2, 4}},
                {{0, 1}, {1, -1}, {2, -2}}});
-  SVector v0 = m.column(0);
-  SVector v2 = m.column(2);
+  const SVector v0 = m.column(0);
+  const SVector v2 = m.column(2);
   m.swap_columns(0, 2);
-  EXPECT_TRUE(m.column(0) == v2);
-  EXPECT_TRUE(m.column(2) == v0);
+  EXPECT_EQ(v2, m.column(0));
+  EXPECT_EQ(v0, m.column(2));
 }
