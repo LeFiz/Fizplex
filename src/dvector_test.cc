33f5e80903b0e3718a13a5d3e0d4b570a6913949
LeFiz <yesmehfriend@gmail.com>
2017-11-08 13:12:45 +0100

    Make vector tests less verbose

--- src/dvector_test.cc
+++ src/dvector_test.cc
@@ -2,139 +2,87 @@
 #include "numeric.h"
 #include "gtest/gtest.h"
 
-TEST(DVectorTest, NewVectorHasCorrectDim) {
-  DVector v(5);
-  EXPECT_TRUE(v.dimension() == 5);
+TEST(DVectorDimensionTest, NewVectorHasCorrectDim) {
+  EXPECT_TRUE(DVector().dimension() == 0);
+  EXPECT_TRUE(DVector(5).dimension() == 5);
 }
 
-TEST(DVectorTest, ResizeToLargerDimLeavesValuesButChangesDim) {
-  DVector v(5);
-  const double d = 3.14;
-  v[0] = d;
-  v.resize(10);
-  EXPECT_TRUE(v.dimension() == 10);
-  EXPECT_TRUE(is_eq(v[0], d));
+class DVectorSingleVectorTest : public ::testing::Test {
+public:
+  DVector vec = {0.0, 1.0, 2.0, 3.0, 4.0};
+};
+
+TEST_F(DVectorSingleVectorTest, ResizeToLargerDimLeavesValuesButChangesDim) {
+  vec.resize(10);
+  EXPECT_EQ(10u, vec.dimension());
+  EXPECT_DOUBLE_EQ(2.0, vec[2]);
 }
 
-TEST(DVectorTest, ResizeToSmallerDimLeavesValuesButChangesDim) {
-  DVector v(5);
-  const double d = 3.14;
-  v[0] = d;
-  v.resize(1);
-  EXPECT_TRUE(v.dimension() == 1);
-  EXPECT_TRUE(is_eq(v[0], d));
+TEST_F(DVectorSingleVectorTest, AppendAddsNewValue) {
+  vec.append(5.0);
+  EXPECT_DOUBLE_EQ(5.0, vec[5]);
 }
 
-TEST(DVectorTest, AppendAddsNewValue) {
-  DVector v(5);
-  const double d = 1.23;
-  v.append(d);
-  EXPECT_TRUE(is_eq(v[5], d));
-}
-
-TEST(DVectorTest, NonConstMemberAccess) {
-  DVector v(5);
-  double d = 3.1;
-  v[2] = d;
-  EXPECT_TRUE(is_eq(v[2], d));
+TEST_F(DVectorSingleVectorTest, RandomAccessAssignmentSetsValue) {
+  vec[2] = 3.14;
+  EXPECT_DOUBLE_EQ(3.14, vec[2]);
 }
 
 TEST(DVectorDotProductTest, ZeroVectors) {
-  DVector v(10), w(10);
-  EXPECT_TRUE(is_zero(v * w));
+  EXPECT_TRUE(is_zero(DVector(10) * DVector(10)));
 }
 
 TEST(DVectorDotProductTest, NonZeroVectors) {
-  DVector v(10), w(10);
-  const double d = 7.123456;
-  v[3] = 1;
-  w[3] = d;
-  v[7] = 0.5;
-  w[7] = 2.0;
-  EXPECT_TRUE(is_eq(v * w, d + 1));
-  EXPECT_TRUE(is_eq(v * w, w * v));
+  const DVector v = {0.5, 0.0, 0.2};
+  const DVector w = {2.0, 4.0, 6.0};
+  EXPECT_DOUBLE_EQ(2.2, v * w);
+  EXPECT_DOUBLE_EQ(v * w, w * v);
 }
 
 TEST(DVectorSVectorDotProductTest, ZeroVectors) {
-  DVector v(10);
-  SVector w;
-  EXPECT_TRUE(is_zero(v * w));
+  EXPECT_DOUBLE_EQ(0.0, DVector(5) * SVector());
 }
 
 TEST(DVectorSVectorDotProductTest, NonZeroVectors) {
-  DVector v(10);
-  SVector w;
-  const double d = 7.123456;
-  v[3] = 1;
-  w.add_value(3, d);
-  v[7] = 0.5f;
-  w.add_value(7, 2.0);
-  EXPECT_TRUE(is_eq(v * w, d + 1));
+  const DVector v = {2.0, 1.0, 0.5};
+  const SVector w = {{0, 0.5}, {2, 2.0}};
+  EXPECT_DOUBLE_EQ(2.0, v * w);
 }
 
 TEST(DVectorTest, EqualityForEmptyVectors) {
-  DVector v, w;
-  EXPECT_TRUE(v == w);
+  EXPECT_TRUE(DVector() == DVector());
 }
 
 TEST(DVectorTest, EqualityForEqualVectors) {
-  DVector v(3), w(3);
-  const double d = 2.123456;
-  v[0] = 1;
-  w[0] = 1;
-  v[2] = d;
-  w[2] = d;
+  const DVector v = {1.0, 2.0, 3.0};
+  const DVector w = {1.0, 2.0, 3.0};
   EXPECT_TRUE(v == w);
-  EXPECT_TRUE(!(v != w));
+  EXPECT_FALSE(v != w);
 }
 
 TEST(DVectorTest, EqualityForInequalVectors) {
-  DVector v(3), w(3);
-  const double d = 2.123456;
-  v[0] = 1;
-  w[0] = 1;
-  v[1] = 17;
-  v[2] = d;
-  w[2] = d;
-  EXPECT_TRUE(!(v == w));
-  EXPECT_TRUE((v != w));
+  const DVector v = {1.0, 2.0, 3.0};
+  const DVector w = {-1.0, -2.0, -3.0};
+  EXPECT_FALSE(v == w);
+  EXPECT_TRUE(v != w);
 }
 
 TEST(DVectorTest, MultiplicationWithScalar) {
-  DVector v(3), w(3);
-  v[0] = 1;
-  w[0] = 2;
-  v[1] = 17.5;
-  w[1] = 35.0;
-  v[2] = 0;
-  w[2] = 0;
-  EXPECT_TRUE(2.0f * v == w);
+  EXPECT_EQ(DVector(2), 0.0 * DVector({1.5, 0.5}));
+  EXPECT_EQ(DVector({3.0, 1.0}), 2.0 * DVector({1.5, 0.5}));
 }
 
 TEST(DVectorTest, OperatorCompoundSubtraction) {
-  DVector v(3), w(3);
-  v[0] = 1;
-  w[0] = 2;
-  v[1] = 17.5;
-  w[1] = 35.0;
-  v[2] = 0;
-  w[2] = 0;
-  w -= v;
-  EXPECT_TRUE(v == w);
+  DVector v = {1.0, 2.0, 3.0};
+  v -= DVector({0.0, 1.0, 2.0});
+  EXPECT_EQ(DVector({1.0, 1.0, 1.0}), v);
 }
 
 TEST(DVectorSVectorTest, OperatorCompoundSubtraction) {
-  DVector v(3), w(3);
-  v[0] = 4;
-  v[1] = 40;
-  v[2] = 0.5;
-  w[0] = 2;
-  w[1] = 35.0;
-  w[2] = 0.75;
-
-  SVector sv = {{0, 2}, {1, 5}, {2, -0.25}};
+  DVector v = {1.0, 2.0, 3.0};
+  const SVector sv = {{0, 0.0}, {1, 1.0}, {2, 2.0}};
   v -= sv;
-  EXPECT_TRUE(v == w);
+  EXPECT_EQ(DVector({1.0, 1.0, 1.0}), v);
 }
 
 TEST(DVectorTest, MaxAbs) {
--- src/svector_test.cc
+++ src/svector_test.cc
@@ -1,14 +1,10 @@
 #include "svector.h"
 #include "gtest/gtest.h"
 
-TEST(SVectorTest, EmptyHasZeolength) {
-  SVector v;
-  EXPECT_TRUE(v.length() == 0);
-}
+TEST(SVectorTest, EmptyHasZeolength) { EXPECT_TRUE(SVector().length() == 0); }
 
 TEST(SVectorTest, NonEmptyLength) {
-  SVector v = {{0, -2}, {1, 3}, {2, 9}, {7, -2.11}};
-  EXPECT_TRUE(v.length() == 4);
+  EXPECT_TRUE(SVector({{0, -2}, {7, -2.11}}).length() == 2);
 }
 
 TEST(SVectorTest, AddValueInsertsNewValue) {
@@ -19,7 +15,7 @@ TEST(SVectorTest, AddValueInsertsNewValue) {
   EXPECT_TRUE(is_eq(v.get_value(3), d));
 }
 
-TEST(SVectorTest, AddValueCanAddSameValueTwice) {
+TEST(SVectorTest, AddValueCanAddSameIndexTwice) {
   SVector v = {{0, -2}, {1, 3}, {2, 9}, {7, -2.11}};
   const double d = 3;
   v.add_value(1, d);
@@ -27,8 +23,8 @@ TEST(SVectorTest, AddValueCanAddSameValueTwice) {
 }
 
 TEST(SVectorTest, ReorderedVectorsAreEqual) {
-  SVector v = {{0, -2}, {1, 3}, {2, 9}, {7, -2.11}};
-  SVector w = {{2, 9}, {0, -2}, {7, -2.11}, {1, 3}};
+  const SVector v = {{0, -2}, {1, 3}, {2, 9}, {7, -2.11}};
+  const SVector w = {{2, 9}, {0, -2}, {7, -2.11}, {1, 3}};
   EXPECT_TRUE(v == w);
   EXPECT_TRUE(w == v);
   EXPECT_TRUE(!(v != w));
@@ -36,8 +32,8 @@ TEST(SVectorTest, ReorderedVectorsAreEqual) {
 }
 
 TEST(SVectorTest, NonEmptyVecIsNotEqualToEmpty) {
-  SVector v = {{0, -2}, {1, 3}, {2, 9}, {7, -2.11}};
-  SVector w;
+  const SVector v = {{0, -2}, {1, 3}, {2, 9}, {7, -2.11}};
+  const SVector w;
   EXPECT_TRUE(!(v == w));
   EXPECT_TRUE(v != w);
 }
@@ -50,18 +46,8 @@ TEST(SVectorTest, ChangeValsThroughIteratorSetsRightVal) {
   EXPECT_TRUE(is_eq(v.get_value(2), 27));
 }
 
-TEST(SVectorTest, CanIterateEmptyVec) {
-  SVector v;
-  for (auto &n : v) {
-    n.value *= 3;
-    EXPECT_TRUE(false);
-  }
-}
-
 TEST(SVectorTest, ScalaMult) {
-  SVector v = {{0, -2}, {1, 3}, {4, 9}};
-  SVector w = {{0, -4}, {1, 6}, {4, 18}};
-  EXPECT_TRUE(2.0 * v == w);
+  EXPECT_TRUE(2.0 * SVector({{0, -2}, {4, 9}}) == SVector({{0, -4}, {4, 18}}));
 }
 
 TEST(SVectorTest, SubscriptUpdatesExistingValues) {
