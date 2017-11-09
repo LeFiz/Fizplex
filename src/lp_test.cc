e3662f82409ebb44cc9a18d364f9de97d69ee57a
LeFiz <yesmehfriend@gmail.com>
2017-11-09 12:47:01 +0100

    Make feasibility tests in LP less verbose

--- src/lp_test.cc
+++ src/lp_test.cc
@@ -1,17 +1,50 @@
 #include "lp.h"
 #include "gtest/gtest.h"
 
-TEST(LPTest, ColumnCount) {
+// TODO structure by case: LPTestRangeRow - b is set properly, logical is set
+
+class LPTestFeasibility : public ::testing::Test {
+public:
   LP lp;
-  EXPECT_TRUE(lp.column_count() == 0);
+  void check_feasible(bool feasible, const std::vector<DVector> &vecs) {
+    for (auto &v : vecs)
+      EXPECT_TRUE(lp.is_feasible(v) == feasible);
+  }
+};
+
+TEST_F(LPTestFeasibility, IsFeasibleForBoundedVarAndRangeRow) {
+  lp.add_column(ColType::Bounded, -1.0, 5);
+  lp.add_row(RowType::Range, -3, 7);
+  lp.add_value(0, 0, 2.0);
+
+  check_feasible(true, {{0}, {-1}, {3.5}});
+  check_feasible(false, {{-1.5}, {4}});
 }
 
-TEST(LPTest, RowCount) {
+TEST_F(LPTestFeasibility, IsFeasibleForFreeVarAndRangeRow) {
+  lp.add_column(ColType::Free, -inf, inf);
+  lp.add_row(RowType::Range, -5, 5);
+  lp.add_value(0, 0, 1.0);
+
+  check_feasible(true, {{0}, {-5}, {5}});
+  check_feasible(false, {{-6}, {6}});
+}
+
+TEST_F(LPTestFeasibility, IsFeasibleForFreeVarAndNonBindingRow) {
+  lp.add_column(ColType::Free, -inf, inf);
+  lp.add_row(RowType::NonBinding, -inf, inf);
+  lp.add_value(0, 0, 1.0);
+
+  check_feasible(true, {{0}, {-5}, {5}, {-inf}, {inf}});
+}
+
+TEST(LPTestEmptyLP, ColumnAndRowCount) {
   LP lp;
+  EXPECT_TRUE(lp.column_count() == 0);
   EXPECT_TRUE(lp.row_count() == 0);
 }
 
-TEST(LPTest, AddColumn) {
+TEST(LPTest, AddColumnIncreasesColumnCount) {
   LP lp;
   lp.add_column(ColType::Bounded, -1.33333, 302);
   EXPECT_TRUE(lp.column_count() == 1);
@@ -113,44 +146,4 @@ TEST(LPTest, AddLogicals) {
 
   for (int i = 0; i < 5; i++)
     EXPECT_TRUE(is_eq(lp.get_value(i, 1 + i), 1));
-}
-
-TEST(LPTest, IsFeasibleForBoundedVarAndRangeRow) {
-  LP lp;
-  lp.add_column(ColType::Bounded, -1.0f, 5);
-  lp.add_row(RowType::Range, 0, 7);
-  lp.add_logicals();
-  lp.add_value(0, 0, 2.0f);
-  EXPECT_TRUE(lp.is_feasible(DVector({0, 0})));
-  EXPECT_TRUE(lp.is_feasible(DVector({1, 0})));
-  EXPECT_TRUE(lp.is_feasible(DVector({3.5, 0})));
-  EXPECT_TRUE(!lp.is_feasible(DVector({-1.5, 0})));
-  EXPECT_TRUE(!lp.is_feasible(DVector({5, 0})));
-  EXPECT_TRUE(!lp.is_feasible(DVector({-1.0f, 0})));
-}
-
-TEST(LPTest, IsFeasibleForFreeVarAndRangeRow) {
-  LP lp;
-  lp.add_column(ColType::Free, -inf, inf);
-  lp.add_row(RowType::Range, -5, 5);
-  lp.add_logicals();
-  lp.add_value(0, 0, 1.0);
-  EXPECT_TRUE(lp.is_feasible(DVector({0, 0})));
-  EXPECT_TRUE(lp.is_feasible(DVector({5, 0})));
-  EXPECT_TRUE(lp.is_feasible(DVector({-5, 0})));
-  EXPECT_TRUE(!lp.is_feasible(DVector({6, 0})));
-  EXPECT_TRUE(!lp.is_feasible(DVector({-6, 0})));
-}
-
-TEST(LPTest, IsFeasibleForFreeVarAndNonBindingRow) {
-  LP lp;
-  lp.add_column(ColType::Free, -inf, inf);
-  lp.add_row(RowType::NonBinding, -inf, inf);
-  lp.add_logicals();
-  lp.add_value(0, 0, 1.0);
-  EXPECT_TRUE(lp.is_feasible(DVector({0, 0})));
-  EXPECT_TRUE(lp.is_feasible(DVector({5, 0})));
-  EXPECT_TRUE(lp.is_feasible(DVector({-5, 0})));
-  EXPECT_TRUE(lp.is_feasible(DVector({inf, 0})));
-  EXPECT_TRUE(lp.is_feasible(DVector({-inf, 0})));
 }
