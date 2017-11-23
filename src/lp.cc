be1713caae970deb32bc245cb8c8063e2002f4d2
LeFiz <yesmehfriend@gmail.com>
2017-11-23 16:00:29 +0100

    Use larger tolerance when checking row feasibility

--- src/lp.cc
+++ src/lp.cc
@@ -165,7 +165,7 @@ bool LP::is_feasible(const DVector &x) const {
     double val = 0.0;
     for (size_t col = 0; col < column_count(); col++)
       val += A.get_value(row, col) * x[col];
-    if (!is_eq_norm(val, b[row], 1e-5, 1e-6)) {
+    if (!is_eq_norm(val, b[row], 1e-4, 1e-5)) {
       Debug(0) << "Row " << row << " failed equality test ("
                << "should be: " << b[row] << ", is " << val << ")\n";
       feasible = false;
