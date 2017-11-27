5c6c81a345cb3354573641a442421949b4fbe275
LeFiz <yesmehfriend@gmail.com>
2017-11-27 12:31:29 +0100

    Make dvector header more readable

--- src/dvector.h
+++ src/dvector.h
@@ -10,20 +10,27 @@ public:
   explicit DVector(size_t dimension = 0);
   DVector(std::initializer_list<double> l);
 
-  const size_t &dimension() const;
-  void resize(size_t new_dim);
-  void append(double val);
-  bool operator==(const DVector &rhs) const;
-  bool operator!=(const DVector &rhs) const;
+  // Member access
   double &operator[](size_t i);
   const double &operator[](size_t i) const;
-  double max_abs() const;
 
+  // Size and size manipulation
+  void append(double val);
+  void resize(size_t new_dim);
+  const size_t &dimension() const;
+
+  // Comparison
+  double max_abs() const;
+  bool operator==(const DVector &rhs) const;
+  bool operator!=(const DVector &rhs) const;
+
+  // Iterator access
   auto begin() { return std::begin(vals); }
   auto end() { return std::end(vals); }
   auto cbegin() const { return std::cbegin(vals); }
   auto cend() const { return std::cend(vals); }
 
+  // Math operators
   double operator*(const DVector &rhs) const;
   double operator*(const SVector &rhs) const;
   DVector &operator-=(const DVector &rhs);
@@ -34,7 +41,8 @@ private:
   size_t dim;
 };
 
-std::ostream &operator<<(std::ostream &os, const DVector &v);
 DVector operator*(const double c, const DVector &rhs);
+
+std::ostream &operator<<(std::ostream &os, const DVector &v);
 
 #endif
