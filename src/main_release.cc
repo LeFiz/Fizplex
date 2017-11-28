98cf815ef5b21b50ad9a66e97459e9fa5093c492
LeFiz <yesmehfriend@gmail.com>
2017-11-28 12:27:54 +0100

    Simple read and solve from command-line

--- src/main_release.cc
+++ src/main_release.cc
@@ -1,9 +1,24 @@
 #include <iostream>
 
 #include "lp.h"
+#include "mpsreader.h"
 #include "simplex.h"
 
-int main() {
-  std::cout << "main main\n";
+int main(int argc, char **argv) {
+  if (argc != 2) {
+    std::cout << "Usage: " << argv[0] << " file.mps\n";
+    return 0;
+  }
+  const std::string filename = argv[1];
+  fizplex::LP lp;
+  try {
+    lp = fizplex::MPSReader::read_lp(filename);
+  } catch (const std::exception &e) {
+    std::cerr << "Could not read file " << filename << "\n";
+    std::cerr << "Reason: " << e.what() << "\n";
+    return 1;
+  }
+  fizplex::Simplex spx(lp, 1);
+  spx.solve();
   return 0;
 }
