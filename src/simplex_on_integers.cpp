a482fc588ae24788c3dc7dc2d560742f1f5c1f79
andrey <andrey.lavq@gmail.com>
2022-01-31 11:14:52 +0800

    extract node and related classes. change from depth-first to breadth-first tree travelsal

--- src/simplex_on_integers.cpp
+++ src/simplex_on_integers.cpp
@@ -6,7 +6,7 @@
 //
 
 #include "simplex_on_integers.h"
-
+#include <queue>
 
 using namespace fizplex;
 
@@ -19,36 +19,45 @@ SimplexOnIntegers::SimplexOnIntegers(LP &a_lp)
 
 void SimplexOnIntegers::solve()
 {
-    bool built = _head->build_tree();
+    std::queue<Node*> tree;
+    tree.push(_head.get());
     
-    if (built) {
+    bool sol_found = false;
+    double cur_best = -inf;
+    DVector solution;
+    
+    while (!tree.empty()) {
+        auto& top_node = tree.front();
+        bool built = top_node->build_tree();
+        
+        if (built)
+        {
+            sol_found = true;
+            auto local_sol = top_node->get_solution();
+            if (cur_best < local_sol.first) {
+                cur_best = local_sol.first;
+                solution = local_sol.second;
+            }
+        }
+        else {
+            if (nullptr != top_node->left_child())
+                tree.push( top_node->left_child().get() );
+            
+            if (nullptr != top_node->right_child())
+                tree.push( top_node->right_child().get() );
+        }
+        
+        tree.pop();
+    }
+    
+    if (sol_found) {
         _head_result = Simplex::Result::OptimalSolution;
-        std::tie(_head_z, _head_x) = _head->get_solution();
+        _head_z = cur_best;
+        _head_x = solution;
     }
     else {
         _head_result = Simplex::Result::Infeasible;
     }
-//    _smx.solve();
-//
-//    _head_result = _smx.get_result();
-//    _head_x = _smx.get_x();
-//    _head_z = - _smx.get_z();
-//
-//    const auto size = _lp_original.column_count();
-//    printf("original lp size : %lu\n", size);
-//    const DVector x = get_x();
-//    for (size_t i=0; i<size; ++i) {
-//        auto v = floor(x[i] + 0.5);
-//
-//        if (abs(v - x[i]) < 1e-3) {
-//            ; // integer, pass
-//        }
-//        else {
-//            assert(!_integer_sln_found);
-//
-            
-//        }
-//    }
 }
 
 const DVector &SimplexOnIntegers::get_x() const
@@ -64,117 +73,4 @@ const double &SimplexOnIntegers::get_z() const
 const Simplex::Result &SimplexOnIntegers::get_result() const
 {
     return _head_result;
-}
-
-
-//=================================================================================================
-SubIntegerLP::SubIntegerLP(const LP* parent_lp)
-: LP(*parent_lp)
-{
-}
-SubIntegerLP::SubIntegerLP(const LP* parent_lp,
-                           size_t edge_idx,
-                           double edge_value,
-                           bool branchLess
-                           )
-: LP(*parent_lp)
-, _parent_lp(parent_lp)
-{
-    DVector coefs;
-    for (size_t i=0; i<column_count(); ++i) {
-        coefs.append(i == edge_idx ? 1.0 : 0.0);
-    }
-    
-    if (branchLess) {
-        double rounded = floor(edge_value);
-        add_row(RowType::LE, -inf, rounded, coefs);        // x0 <= floor(x)
-    }
-    else {
-        double rounded = ceil(edge_value);
-        add_row(RowType::GE, rounded, inf, coefs);        // x0 >= ceil(x)
-    }
-    
-//    _local_orig_lp = (LP)*this;
-}
-SubIntegerLP::SubIntegerLP(const SubIntegerLP& other)
-: LP(other)
-, _parent_lp(other._parent_lp)
-{
-    
-}
-
-//=================================================================================================
-Node::Node(SubIntegerLP sub_lp, Node* parent, size_t orig_size, uint32_t deep_level)
-: _sub_lp(sub_lp)
-, _backup_lp(sub_lp)
-, _orig_size(orig_size)
-, _deep_level(deep_level)
-, _parent(parent)
-{
-    printf("Node level of %u created\n", deep_level);
-}
-
-bool Node::build_tree()
-{
-    auto subSmx = Simplex(_sub_lp);
-    
-    subSmx.solve();
-    
-    ////// prints
-    auto res_left = subSmx.get_result();
-    auto subx = subSmx.get_x();
-    printf("result %i\n", res_left);
-    printf("sub obj: %f\n", -subSmx.get_z());
-    for (size_t j=0; j<_orig_size; j++) {
-        printf("%f\n", subx[j]);
-    }
-    ///////
-    
-    if (res_left != Simplex::Result::OptimalSolution) {
-        return false;
-    }
-    
-    bool integer_sol = true;
-    size_t none_integer_idx = 0;
-    const auto& x = subSmx.get_x();
-    for (size_t i=0; i<_orig_size; ++i) {
-        auto v = floor(x[i] + 0.5);
-        
-        if (abs(v - x[i]) < 1e-3) {
-            ; // integer, pass
-        }
-        else {
-            integer_sol = false;
-            none_integer_idx = i;
-            break;
-        }
-    }
-    
-    if (integer_sol) {
-        _obj = - subSmx.get_z();
-        _x = subSmx.get_x();
-        
-        return true;
-    }
-    
-    return create_children(none_integer_idx, x[none_integer_idx]);
-}
-
-bool Node::create_children(size_t idx, double value)
-{
-    if (_deep_level >= MAX_DEEP) {
-        return false;
-    }
-    
-    printf("LEFT : \n");
-    auto subLP_left = SubIntegerLP(&_backup_lp, idx, value, true);
-    _left = std::make_unique< Node >(subLP_left, this, _orig_size, _deep_level + 1);
-    bool left_built = _left->build_tree();
-
-    printf("RIGHT : \n");
-    auto subLP_right = SubIntegerLP(&_backup_lp, idx, value, false);
-    _right = std::make_unique< Node >(subLP_right, this, _orig_size, _deep_level + 1);
-    bool right_built = _right->build_tree();
-
-    return left_built || right_built;
 }
--- src/simplex_on_integers.h
+++ src/simplex_on_integers.h
@@ -9,75 +9,11 @@
 #define simplex_on_integers_hpp
 
 #include "simplex.h"
+#include "node_on_integer.h"
 
 namespace fizplex
 {
 
-static uint32_t MAX_DEEP = 12;
-
-class SubIntegerLP : public LP
-{
-public:
-    
-    SubIntegerLP(const LP* parent_lp);
-    SubIntegerLP(const LP* parent_lp, size_t edge_idx, double edge_value, bool branchLess);
-    SubIntegerLP(const SubIntegerLP&);
-    SubIntegerLP(SubIntegerLP&&) = delete;
-    SubIntegerLP* operator=(const SubIntegerLP&) = delete;
-    SubIntegerLP* operator=(SubIntegerLP&&) = delete;
-    
-private:
-    
-//    LP _local_orig_lp;
-    const LP *_parent_lp;
-};
-
-class Node
-{
-public:
-    Node(SubIntegerLP, Node* parent, size_t orig_size, uint32_t deep_level);
-    
-    bool build_tree();
-    
-    std::pair<double, DVector> get_solution() const {
-        double z_left = -inf;
-        DVector x_left;
-        if (_left != nullptr) {
-            std::tie(z_left, x_left) = _left->get_solution();
-        }
-        if (_right != nullptr) {
-            auto res_right = _right->get_solution();
-            
-            if (res_right.first > z_left) {
-                return res_right;
-            }
-            else {
-                return std::make_pair(z_left, x_left);
-            }
-        }
-        return std::make_pair(_obj, _x);
-    }
-    
-private:
-    
-    bool create_children(size_t idx, double value);
-    
-private:
-    
-    SubIntegerLP _sub_lp;
-    const SubIntegerLP _backup_lp;
-    
-    size_t _orig_size;
-    uint32_t _deep_level;
-    
-    Node* _parent;
-    
-    std::unique_ptr<Node> _left = nullptr;
-    std::unique_ptr<Node> _right = nullptr;
-    
-    double _obj = -inf;
-    DVector _x;
-};
 
 class SimplexOnIntegers
 {
@@ -95,7 +31,7 @@ private:
     
     const LP _lp_original;
 //    Simplex _smx;
-    std::unique_ptr<Node> _head;
+    std::unique_ptr<Node> _head = nullptr;
     
     Simplex::Result _head_result = Simplex::Result::Unsolved;
     DVector _head_x;
